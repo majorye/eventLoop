19458e1596dae62470190917b3cec6235be4b003
MajorYe <major.yezhouquan@gmail.com>
2011-11-30 16:17:48 +0800

    implement the simple watcher event loop, use watcher.watchUtil(watchAttr,viewModel,function(){}).do(function(){}) to watch the target attribute in the target viewModel, also add test case, which integrate with Observer.js

--- testcase/lib/watcher.js
+++ testcase/lib/watcher.js
@@ -1,31 +1,33 @@
 function watcher(){};
-var _wproto=watcher.prototype;
 watcher.watchQueue=[];
+watcher.intervalTime=100;
 /**
 * @value {JSON} {attribute:attr,viewModel:viewModel,fct:fct,callback:callback}
 */
 watcher.addWatchTarget=function(obj){
   watcher.watchQueue.push(obj);
 }
+
 watcher.removeWatchTarget=function(key){
   //todo
 }
 watcher.startWatch=function(){
   if(!this.intervalId){
     this.intervalId=setInterval(function(){
-	  console.log("++++"+(new Date).getTime()+"++++");
-	  for(var i=0;i<watcher.watchQueue.length;i++){
-	    var obj=watcher.watchQueue[i];
-		setTimeout(function(){
-		  watcher.playback(obj);
-		},10000);
-	  }
-	},0);
+		  for(var i=0;i<watcher.watchQueue.length;i++){
+			var obj=watcher.watchQueue[i];
+			setTimeout(function(){
+			  watcher.playback(obj);
+			},0);
+		  }
+	},watcher.intervalTime);
   }
 }
 
 watcher.stopWatch=function(){
-  window.clearInterval(this.intervalId);
+  if(this.intervalId){
+    window.clearInterval(this.intervalId);
+  }
 };
 
 watcher.playback=function(playbackObj){
@@ -34,20 +36,21 @@ watcher.playback=function(playbackObj){
  var viewModel=p.viewModel;
  var fct=p.fct;
  var callback=p.callback;
- if(fct.apply(viewModel,[attr])){
+ if(fct.apply(viewModel,[attribute])){
    callback();
  }
 }
+
 // how to delete watch target? 
 // use UID as store key?
 // how to watch object which has too much level?
 watcher.watchUtil=function(attr,viewModel,fct){
    return {
-     do:function(f){
-	   watcher.addWatchTarget(viewModel.uid,{attribute:attr,viewModel:viewModel,fct:fct,callback:f});
+     'do':function(f){
+	   watcher.addWatchTarget({attribute:attr,viewModel:viewModel,fct:fct,callback:f});
 	   if(fct.apply(viewModel,[attr])){
 	     f();
 	   }
-	 };
+	 }
    };
 }
